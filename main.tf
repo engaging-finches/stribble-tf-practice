c21173d390186f557d70adf785c6a6707bf9e5f4
Sloane Tribble <sloane.tribble@liatrio.com>
2024-04-29 15:10:12 -0700

    Create gitignore

---
+++ main.tf
@@ -0,0 +1,44 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  cloud {
+    organization = "finches"
+    workspaces {
+      name = "learn-terraform-azure-stribble"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+# configure specified provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "stribbleTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+
