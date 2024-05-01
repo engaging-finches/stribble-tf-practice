3fdbf192960b0e1898d132bc6df8b12c2a240ead
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-01 11:52:04 -0700

    fix: Format main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   # }
 
   backend "azurerm" {
-    resource_group_name  = "RG-SLOANE_TRIBBLE-bootcamp"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "stribblestorage"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "RG-SLOANE_TRIBBLE-bootcamp" # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "stribblestorage"            # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"     # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 
   required_version = ">= 1.1.0"
