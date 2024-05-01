439b379cf64a7265ca2d930007652fc4c82c8bee
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-01 16:51:44 -0700

    feat: run fmt

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ terraform {
     storage_account_name = "calvinsa"
     container_name       = "calvintfstate"
     key                  = "terraform.tfstate"
-    subscription_id = "3e16852e-8399-4c16-b246-16bf46bc3747"
+    subscription_id      = "3e16852e-8399-4c16-b246-16bf46bc3747"
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  subscription_id = "3e16852e-8399-4c16-b246-16bf46bc3747"
+  subscription_id            = "3e16852e-8399-4c16-b246-16bf46bc3747"
   features {}
 }
 
