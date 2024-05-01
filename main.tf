d633059c8f10a13ee85a64e9bc24c75f7263fa14
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-01 12:25:00 -0700

    feat: Update backend config to use oidc

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ terraform {
     storage_account_name = "stribblestorage"            # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"     # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    use_oidc             = true
   }
 
   required_version = ">= 1.1.0"
