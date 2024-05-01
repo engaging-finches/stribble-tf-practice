34eecec74ffeb6c6222895c03c345736964cae51
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-01 12:21:32 -0700

    fix: Remove non-functioning stuff

--- main.tf
+++ main.tf
@@ -27,10 +27,7 @@ terraform {
 # configure specified provider
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  
 }
 
 # resource "azurerm_resource_group" "rg" {
