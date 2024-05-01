6a0bfbc7a872439313c0818f3850449f852cc7ae
Sloane Tribble <sloane.tribble@liatrio.com>
2024-05-01 12:28:26 -0700

    feat: Add back env vars to main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,10 @@ terraform {
 # configure specified provider
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
   
 }
 
